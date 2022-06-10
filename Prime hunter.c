4ca52926c0ef2ab0c11dfd964824f565060ebe1a
Felkng <105019177+Felkng@users.noreply.github.com>
2022-06-10 08:48:05 -0300

    Update Prime hunter.c

--- Prime hunter.c
+++ Prime hunter.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int underln=0, overln=0, prime=0, min=0, max=0, ctloop=0;
+    int underln=0, overln=0, prime=0, min=0, max=0;
     scanf("%d%d",&min,&max); //DÍGITE O NUMERO MIN E O MAXIMO, RESPECTIVAMENTE, O PROGRAMA VAI VARRER TODOS OS NUMEROS PRIMOS DENTRO DESSE INTERVALO
     overln=min;
     while(overln<max)
